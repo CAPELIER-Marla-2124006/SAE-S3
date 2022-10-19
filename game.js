a6e11cfb6c5e4d950584526e83dbcf8a8eebd18b
Capelier-Marla <marla.capelier@etu.univ-amu.fr>
2022-10-19 15:43:25 +0200

    we can now create platforms where we can pass thru from bottom

--- game.js
+++ game.js
@@ -24,28 +24,38 @@ var game = new Phaser.Game(config)
 let player          // playable object
 let cursors         // keyboard cursors to move
 let grounds         // grounds where player can move
+let groundList = {}
+let platforms
+let platformList = {}// platforms where we can pas thru
 let colliders = {}  // array of colliders
-let debugNB = 0
 
 function preload() {
     this.load.image('player', 'assets/images/player.png')
     this.load.image('ground', 'assets/images/ground.png')
+    this.load.image('platform', 'assets/images/platform.png')
 }
 
 function create() {
     player = this.physics.add.image(100, 100, 'player')
     player.setCollideWorldBounds(true)
-    player.body.checkCollision.up = false
 
     cursors = this.input.keyboard.createCursorKeys()
 
+    // create the ground
     grounds = this.physics.add.staticGroup()
-    // create ground
-    grounds.create(config.width/2, config.height-(32/2*1.7), 'ground').setScale(1.7).refreshBody()
-    // create another ground
-    grounds.create(config.width*3/4, config.height*2/3, 'ground')
-
+    groundList["bottom"] = grounds.create(config.width/2, config.height-(32/2*1.7), 'ground').setScale(1.7).refreshBody()
+    groundList["top"] = grounds.create(config.width*(4/5), config.height*(2/5), 'ground').setScale(0.8).refreshBody()
     colliders["player-ground"] = this.physics.add.collider(player, grounds)
+
+    // create platforms
+    platformList["first-platform"] = this.physics.add.staticSprite(config.width*(3/4), config.height*(2/3), 'platform').refreshBody()
+    // create collisions with the platforms and player
+    for (const key of Object.keys(platformList)) {
+        platformList[key].body.checkCollision.down = false
+        platformList[key].body.checkCollision.left = false
+        platformList[key].body.checkCollision.right = false
+        colliders[key] = this.physics.add.collider(player, platformList[key])
+    }
 
 }
 
