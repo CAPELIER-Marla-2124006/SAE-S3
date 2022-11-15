fd1466e0966cf0afcc3f6d21f340951e18975b0d
ZeKap <kapismath@gmail.com>
2022-11-15 22:22:46 +0100

    almost working  presentation (db ok)

--- sql.php
+++ sql.php
@@ -1,19 +1,22 @@
 <?php
 require "db.php";
 
+//debug
+//echo("P " . $_POST['request']. PHP_EOL . "G " . $_GET['request']. PHP_EOL . "R " . $_REQUEST['request']);
+
 // get request
-if(isset($_POST['request'])) $request = $_POST['request'];
+if(isset($_REQUEST['request'])) $request = $_REQUEST['request'];
 else {
-    echo('Roquette vide');
+    echo('Roquette REQUEST vide');
     exit();
 }
 
 // query db
-$ps = $db->prepare($request);
-$ps->execute();
+$response = $db->prepare($request);
+$response->execute();
 
 // encode repsonse in json and store in result
-$result = json_encode($ps->fetchAll());
+$result = json_encode($response->fetchAll());
 
 echo($result);
 ?>
