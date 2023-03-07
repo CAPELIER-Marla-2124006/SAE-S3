1484b87d5192f8be7a0af13966f1186be08495aa
Romain CLEMENT <rc2301@orange.fr>
2023-03-07 10:09:28 +0100

    Reject abstract, return to interface, it's better

--- Model/DataAccess.php
+++ Model/DataAccess.php
@@ -1,6 +1,6 @@
 <?php
 
-class DataAccess extends ADataAccess {
+class DataAccess implements DataAccessInterface {
 
 	private $data;
 
--- Model/DataAccessInterface.php
+++ Model/DataAccessInterface.php
@@ -1,20 +1,14 @@
 <?php
 
-abstract class ADataAccess
+interface DataAccessInterface
 {
-
-    /**
-     * Prepare all statements needed for the other abstract methods
-     * The function is only called by the constructor of the abstract class
-     */
-    public abstract function prepareStatements(): void;
 
     /**
      * Get an exercise by its id in the database
      * @param int $id The id of the exercise
      * @return Exercise An instance that holds exercise attributes
      */
-    public abstract function getExercise(int $id): Exercise;
+    public function getExercise(int $id): Exercise;
 
     /**
      * Check if an user exists in the database
@@ -23,19 +17,19 @@ abstract class ADataAccess
      * @return bool Returns true if the pair (username, password) exists in the
      * USER table (false in the other case)
      */
-    public abstract function isUser(string $username, string $password): bool;
+    public function isUser(string $username, string $password): bool;
 
     /**
      * Get an user by its username in the database
      * @param string $username The username of the user
      * @return User An instance that holds user attributes
      */
-    public abstract function getUser(string $username): User;
+    public function getUser(string $username): User;
 
     /**
      * Update an user in the database
      * @param User $user The model to take values
      */
-    public abstract function updateUser(User $user): void;
+    public function updateUser(User $user): void;
 
 }
