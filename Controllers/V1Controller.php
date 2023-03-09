d1ee688778c81763a98c04cfc3aa4201cb4fe938
Nicolas <nicolas.laurent@etu.univ-amu.fr>
2023-03-09 14:07:00 +0100

    edit V1Controller

--- Controllers/V1Controller.php
+++ Controllers/V1Controller.php
@@ -2,24 +2,41 @@
 
 class V1Controller extends AController {
 
-    public function process() {
-        switch ($this->urlFolder) {
-            case 'hint':{
+    public function process() { // TODO FIX DATAACCESS
+        switch ($this->urlFolder) { 
+            case 'exercise':{                   
+                $data = new DataAccess();
+                $exercice = $data->getExercise($this->urlParams[0]);//urlParams[0] = nb level
+                $php_array = [
+                    'lesson'=>$exercice->getLesson(),
+                    'instructions'=>$exercice->getInstructions(),
+                    'hint'=>$exercice->getHint(),
+                    'codeInit'=>$exercice->getCodeInit(),
+                    'points'=>$exercice->getPoints(),
+                    'success'=>$exercice->getSuccess()
+                ];
+                $json_array = json_encode($php_array);
+                echo($json_array);
                 break;
             }
-
-            case 'lessons':{
-                echo "Lesson";
+            case 'saveUser':{
+                $data = new DataAccess();
+                $user = $data->getUser($_SESSION['ID']);
+                $user->setNotes($this->postParams['notes']);
+                $user->setColorHue($this->postParams['colorHue']);
+                $data->updateUser($user);
                 break;
             }
+            case 'submit':{//points et user level
+                /////// PSEUDO CODE ////////
+                $dataExercise = new DataAccessUser(); // connect as not Admin
+                $dataAdmin = new DataAccessAdmin(); // connect as Admin
 
-            case 'notes':{
-                echo "Notes";
-                break;
-            }
-
-            case 'instructions':{
-                echo "Instructions";
+                $rightAnswer = $dataAdmin->getExercise($this->urlParams[0])->getAnswer();
+                
+                /*$user = $data->getUser($_SESSION['ID']);
+                $user->setPoints($this->postParams['points']);
+                $user->setLevel($this->postParams['level']);*/
                 break;
             }
             default:{
