dac888e4d4a90168854440e5353c441d32c528cf
Nicolas <nicolas.laurent@etu.univ-amu.fr>
2023-03-09 17:26:59 +0100

    edit submit V1Controller

--- Controllers/V1Controller.php
+++ Controllers/V1Controller.php
@@ -12,7 +12,6 @@ class V1Controller extends AController {
                     'instructions'=>$exercice->getInstructions(),
                     'hint'=>$exercice->getHint(),
                     'codeInit'=>$exercice->getCodeInit(),
-                    'points'=>$exercice->getPoints(),
                     'success'=>$exercice->getSuccess()
                 ];
                 $json_array = json_encode($php_array);
@@ -22,8 +21,15 @@ class V1Controller extends AController {
             case 'saveUser':{
                 $data = new DataAccess();
                 $user = $data->getUser($_SESSION['ID']);
-                $user->setNotes($this->postParams['notes']);
-                $user->setColorHue($this->postParams['colorHue']);
+                if(isset($this->postParams['notes'])) {
+                    $user->setNotes($this->postParams['notes']);
+                }
+                if(isset($this->postParams['colorHue'])) {
+                    $user->setColorHue($this->postParams['colorHue']);
+                }
+                if(isset($this->postParams['points'])) {
+                    $user->setPoints($this->postParams['points']);
+                }
                 $data->updateUser($user);
                 break;
             }
@@ -33,6 +39,60 @@ class V1Controller extends AController {
                 $dataAdmin = new DataAccessAdmin(); // connect as Admin
 
                 $rightAnswer = $dataAdmin->getExercise($this->urlParams[0])->getAnswer();
+
+                $userAnswer = $this->postParams['answer'];
+
+                if($userResult = $dataExercise->getResult($userAnswer) == $dataAdmin->getResult($rightAnswer)){
+                    $win = ['true'];
+                    $user = $dataAdmin->getUser($_SESSION['ID']);
+                    $exercice = $dataAdmin->getExercise($this->urlParams[0]);
+                    $points = [$user->getPoints() + $exercice->getPoints()];
+
+                    // create table and headers of table
+                    $table = "<table><tr>";
+                    // for each keys in first result
+                    foreach ($userResult[0] as $key => $var) {
+                        // if it's not a number
+                        if(!ctype_digit($key) && !is_numeric($key)) $table += "<th>".$key."</th>";
+                    }
+                    $table += "</tr>";
+                
+                    // fill table
+                    // for each line in results
+                    foreach ($userResult as $line) {
+                        $table += "<tr>";
+                        // for each case in line
+                        foreach ($line as $key => $case) {
+                            // if it's not a number
+                            if(!ctype_digit($key) && !is_numeric($key)) $table += "<td>".$case."</td>";
+                        }
+                        $table += "</tr>";
+                    }
+                    //end table
+                    $table += "</table>";
+                    
+                    $win = json_encode($win);
+                    echo($win);
+                    $points = json_encode($points);
+                    echo($points);
+                    $table = json_encode($table);
+                    echo($table);
+                }
+                else{
+                    $win = ['false'];
+                    $user = $dataAdmin->getUser($_SESSION['ID']);
+                    $exercice = $dataAdmin->getExercise($this->urlParams[0]);
+                    $points = [$user->getPoints() - intdiv($exercice->getPoints(), 5)];
+                    $table = "Aucun resultat";
+                    
+                    $win = json_encode($win);
+                    echo($win);
+                    $points = json_encode($points);
+                    echo($points);
+                    $table = json_encode($table);
+                    echo($table);
+                }
+                // commande romain dataaccess pour récupérer les deux réponse, et les comparer(dans le controller)
                 
                 /*$user = $data->getUser($_SESSION['ID']);
                 $user->setPoints($this->postParams['points']);
