7e3679aca7fb3eee3177f9575b4e7bc1b6da4099
Nicolas <nicolas.laurent@etu.univ-amu.fr>
2023-03-20 16:17:49 +0100

    add doc in Model, Session and View

--- Kernel/View.php
+++ Kernel/View.php
@@ -2,6 +2,12 @@
 
 final class View {
 
+    /**
+     * Show the view
+     * @param string $path path of the view
+     * @param array $params parameters of the view
+     * @return void
+     */
     public static function show (string $path, Array $params = array()): void
     {
         $S_file = Constants::viewsDir() . $path . '.php';
