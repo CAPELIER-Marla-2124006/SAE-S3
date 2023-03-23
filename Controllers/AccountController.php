0eefbe6dbfebb95c4ef170df85dd57e27ba64bdb
Capelier-Marla <marla.capelier@etu.univ-amu.fr>
2023-03-23 18:55:07 +0100

    added account controller file

---
+++ Controllers/AccountController.php
@@ -0,0 +1,19 @@
+<?php
+
+class AccountController extends AController
+{
+
+	public function process(){
+		switch ($this->urlFolder) {
+			case 'login':{
+				break;
+			}
+			case 'register':{
+				break;
+			}
+			case 'disconnect':{
+				break;
+			}
+		}
+	}
+}
