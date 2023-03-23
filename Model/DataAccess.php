4007dccaad003b9a7edc6865f46d139288171691
Nicolas <nicolas.laurent@etu.univ-amu.fr>
2023-03-23 20:33:34 +0100

    DataAccess fixed

--- Model/DataAccess.php
+++ Model/DataAccess.php
@@ -13,10 +13,11 @@ class DataAccess implements DataAccessInterface {
 	 * @param int $id The id of the exercise
 	 * @return Exercise An instance that holds exercise attributes
 	 */
-	 public function getExercise(int $id): Exercise {
+	 public function getExercise(int $id): Exercise|null {
 		$statement = $this->data->prepare('SELECT * FROM APP_EXERCISE WHERE ID=?');
 		$statement->execute([$id]);
 		$row = $statement->fetch();
+		if($row == null) return null;
 		return new Exercise(
 			$row['ID'], $row['CODE_INIT'], $row['INSTRUCTION'], $row['LESSON'],
 			$row['HINT'], $row['SUCCESS'], $row['ANSWER'], $row['POINTS']
@@ -24,27 +25,15 @@ class DataAccess implements DataAccessInterface {
 	}
 
 	/**
-	 * Check if an user exists in the database
-	 * @param string $username The username to check
-	 * @param string $password The password to check
-	 * @return bool Returns true if the pair (username, password) exists in the
-	 * USER table (false in the other case)
-	 */
-	public function isUser(string $username, string $password): bool {
-		$statement = $this->data->prepare('SELECT * FROM APP_USER WHERE USERNAME=? AND PASSWORD=?');
-		$statement->execute([$username, $password]);
-		return $statement->rowCount() != 0;
-	}
-
-	/**
 	 * Get an user by its username in the database
 	 * @param string $username The username of the user
 	 * @return User An instance that holds user attributes
 	 */
-	public function getUser(string $username): User {
+	public function getUser(string $username): User|null {
 		$statement = $this->data->prepare('SELECT * FROM APP_USER WHERE USERNAME=?');
 		$statement->execute([$username]);
 		$row = $statement->fetch();
+		if($row == null) return null;
 		return new User($row['USERNAME'], $row['PASSWORD'], $row['NOTES'],
 			$row['LEVEL'], $row['COLOR_HUE'], $row['POINTS']);
 	}
@@ -55,8 +44,9 @@ class DataAccess implements DataAccessInterface {
      * @param string $username The new username
      * @return User A new user holding new data
      */
-    public function updateUsername(User $user, string $username): User {
-		$this->data->prepare('UPDATE APP_USER SET USERNAME=? WHERE USERNAME=?')->execute([$username, $user->getUsername()]);
+    public function updateUsername(User $user, string $username): User|null {
+		$row = $this->data->prepare('UPDATE APP_USER SET USERNAME=? WHERE USERNAME=?')->execute([$username, $user->getUsername()]);
+		if($row == null)return null;
 		return new User($username, $user->getPassword(), $user->getNotes(),
 			$user->getLevel(), $user->getColorHue(), $user->getPoints());
 	}
@@ -72,7 +62,22 @@ class DataAccess implements DataAccessInterface {
 			'notes' => $user->getNotes(),
 			'level' => $user->getLevel(),
 			'hue' => $user->getColorHue(),
-			'points' => $user->getPoints()
+			'pts' => $user->getPoints()
+		]);
+	}
+
+	/**
+	 * Insert an user in the database
+	 * @param User $user The model to take values
+	 */
+	public function insertUser(User $user): void {
+		$this->data->prepare('INSERT INTO APP_USER (USERNAME, PASSWORD, NOTES, LEVEL, COLOR_HUE, POINTS) VALUES (:username, :password, :notes, :level, :hue, :pts)')->execute([
+			'username' => $user->getUsername(),
+			'password' => $user->getPassword(),
+			'notes' => $user->getNotes(),
+			'level' => $user->getLevel(),
+			'hue' => $user->getColorHue(),
+			'pts' => $user->getPoints()
 		]);
 	}
 
