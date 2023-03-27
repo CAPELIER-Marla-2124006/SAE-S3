03e450daaebbef87ac4946e8e9e1f82fa6e98770
Capelier-Marla <marla.capelier@etu.univ-amu.fr>
2023-03-27 17:07:52 +0200

    remove code from first semester

--- php/gitSync.php
+++
@@ -1,6 +0,0 @@
-<?php
-// nothing to see here, this page has nothing to do except it's used by a github action to sync the server every new push so it's always in sync with the git repo
-$ex = shell_exec("git pull");
-//$ex = shell_exec("id; whoami");
-echo($ex);
-?>
